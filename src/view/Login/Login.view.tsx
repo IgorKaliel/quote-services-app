fba025f4270b839f547fb72c0d74768af1d05955
Igor Kaliel <igorkaliel@gmail.com>
2026-03-27 14:07:50 -0300

    FE-0003/@feature/login-ui: add login ui with login mvvm and mutation

--- src/view/Login/Login.view.tsx
+++ src/view/Login/Login.view.tsx
@@ -4,8 +4,15 @@ import { AppInput } from "@/shared/components/organisms/AppInput"
 import { AuthFormHeader } from "@/shared/components/molecules/AppFormHeader"
 import { AppButton } from "@/shared/components/atoms/AppButton"
 import { KeyboardContainer } from "@/shared/components/organisms/KeyboardContainer"
+import { FC } from "react"
+import { useLoginViewModel } from "./useLogin.viewModel"
+import { AppInputController } from "@/shared/components/molecules/AppInputController"
 
-export const LoginView = () => {
+export const LoginView: FC<ReturnType<typeof useLoginViewModel>> = ({
+  control,
+  errors,
+  onSubmit,
+}) => {
   return (
     <KeyboardContainer>
       <View className="flex-1 px-[40px] justify-between items-center">
@@ -15,33 +22,36 @@ export const LoginView = () => {
             title="Acesse sua conta"
           />
 
-          <AppInput label="E-MAIL" leftIcon="mail-outline" />
+          <AppInputController
+            control={control}
+            name="email"
+            label="E-MAIL"
+            leftIcon="mail-outline"
+          />
 
-          <AppInput
+          <AppInputController
+            control={control}
+            name="password"
             label="SENHA"
             leftIcon="lock-closed-outline"
             secureTextEntry
           />
 
-          <View className="flex-1 w-full items-end mt-2">
-            <TouchableOpacity
-              onPress={() => router.push("/(public)/forgot-password")}
-            >
-              <Text className="text-sm text-purple-base font-bold">
-                Esqueceu a senha?
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View className="w-full items-end mt-2">
+          <View className="w-full items-end mt-1">
             <TouchableOpacity
               onPress={() => router.push("/(public)/forget-password")}
+              className="px-1 py-1"
             >
               <Text className="text-sm text-purple-base font-bold">
                 Esqueceu a senha?
               </Text>
             </TouchableOpacity>
           </View>
-          <AppButton className="mt-6" rightIcon="arrow-forward">
+          <AppButton
+            onPress={onSubmit}
+            className="mt-5"
+            rightIcon="arrow-forward"
+          >
             Entrar
           </AppButton>
         </View>
